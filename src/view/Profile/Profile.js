620c8fb64342e631826380401898ea0b5ef8f75d
vuongvitin <vuong@gmail.com>
2022-05-22 16:22:54 +0700

    22/5

--- src/view/Profile/Profile.js
+++ src/view/Profile/Profile.js
@@ -1,18 +1,26 @@
 import { Grid } from '@mui/material'
-import React from 'react'
+import React, { useContext } from 'react'
 import { Card, Container, Spinner } from 'react-bootstrap'
+import { UserContext } from '../../App';
 
-const Profile = () => {
+const Profile = () => { 
+    const src = 'https://res.cloudinary.com/dwsyse8jk/image/upload/v1630359671/t02qq60zya71rmrg5gfe.jpg';
+    const [user, dispatch] = useContext(UserContext)
     return (
         <Container>
             <p className="text-center">
-                <Spinner variant="info" animation="border" role="status" />
+                <h1>Thông tin người dùng {user.last_name}</h1>
             </p>
-            <Grid xs="12">
+            <Grid container xs="12">
                 <Grid xs="4">
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src={src} />
                     </Card>
+                </Grid>
+                <Grid xs="8">
+                    <p>{user.first_name}</p>
+                    <p>{user.last_name}</p>
+                    <p>{user.email}</p>
                 </Grid>
             </Grid>
             
